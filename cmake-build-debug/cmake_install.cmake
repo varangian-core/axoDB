261638406400006fdd3f7db54e14e71cbbc12027
varangian-core <96369233+varangian-core@users.noreply.github.com>
2023-09-27 23:29:10 -0700

    CMake changes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -37,6 +37,14 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Users/Yormingandr/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/232.9559.58/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/Yormingandr/CLionProjects/axoDB/cmake-build-debug/src/cmake_install.cmake")
+  include("C:/Users/Yormingandr/CLionProjects/axoDB/cmake-build-debug/tests/cmake_install.cmake")
+  include("C:/Users/Yormingandr/CLionProjects/axoDB/cmake-build-debug/_deps/googletest-build/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
