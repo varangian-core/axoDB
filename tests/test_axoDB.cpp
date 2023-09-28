78d42aaa1c291e304a8a4df3aefc1a3e2520da66
varangian-core <96369233+varangian-core@users.noreply.github.com>
2023-09-27 23:43:56 -0700

    More comprehensive test

--- tests/test_axoDB.cpp
+++ tests/test_axoDB.cpp
@@ -1,18 +1,19 @@
-// test_axoDB.cpp
-
+//
+// Created by Yormingandr on 9/26/2023.
+//
 #include "../src/common/Allocator.h"
 #include <gtest/gtest.h>
 
 // Define a fixture for the tests
 class AllocatorTest : public ::testing::Test {
 protected:
+    // Create a new allocator for each test
     void SetUp() override {
-        // Create a new allocator for each test
         allocator = new axodb::BasicAllocator();
     }
 
+    // Delete the allocator after each test
     void TearDown() override {
-        // Delete the allocator after each test
         delete allocator;
     }
 
@@ -21,11 +22,9 @@ protected:
 
 // Test the Allocate and Deallocate methods
 TEST_F(AllocatorTest, AllocateAndDeallocate) {
-    // Arrange
     axodb::IndexType size = 10;
     axodb::DataPointer ptr = allocator->Allocate(size);
 
-    // Act
     // Check that the pointer is not null
     ASSERT_NE(ptr, nullptr);
 
@@ -41,7 +40,46 @@ TEST_F(AllocatorTest, AllocateAndDeallocate) {
 
     allocator->Deallocate(ptr, size);
 
-    // Assert
     // Check that the pointer is null after deallocation
     ASSERT_EQ(ptr, nullptr);
+}
+
+// Test the Allocate method with a size of zero
+TEST_F(AllocatorTest, AllocateZeroSize) {
+    axodb::IndexType size = 0;
+    axodb::DataPointer ptr = allocator->Allocate(size);
+
+    // Check that the pointer is null
+    ASSERT_EQ(ptr, nullptr);
+}
+
+// Test the Allocate method with a negative size
+TEST_F(AllocatorTest, AllocateNegativeSize) {
+    axodb::IndexType size = -1;
+    axodb::DataPointer ptr = allocator->Allocate(size);
+
+    // Check that the pointer is null
+    ASSERT_EQ(ptr, nullptr);
+}
+
+// Test the Deallocate method with a null pointer
+TEST_F(AllocatorTest, DeallocateNullPointer) {
+    axodb::IndexType size = 10;
+    axodb::DataPointer ptr = nullptr;
+
+    allocator->Deallocate(ptr, size);
+
+    // Check that the Deallocate method does not crash with a null pointer
+    SUCCEED();
+}
+
+// Test the Deallocate method with a negative size
+TEST_F(AllocatorTest, DeallocateNegativeSize) {
+    axodb::IndexType size = -1;
+    axodb::DataPointer ptr = reinterpret_cast<axodb::DataPointer>(1);
+
+    allocator->Deallocate(ptr, size);
+
+    // Check that the Deallocate method does not crash with a negative size
+    SUCCEED();
 }
